a6dd0cc7345ea9f9b6d11513160988b2d145a4c3
Alejandro Dionisio <adionisioburillo@gmail.com>
2018-07-29 22:28:04 +0200

    first commit

---
+++ app/src/main/java/com/adionisio/rssreader/ArticlesListActivity.java
@@ -0,0 +1,13 @@
+package com.adionisio.rssreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticlesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articles_list);
+    }
+}
